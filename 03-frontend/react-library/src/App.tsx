044d7b3f4d3295b184892428e2db56d22e3e8454
rajeev1209 <kumar.rajeevranjan10@yahoo.com>
2023-12-21 23:07:53 -0500

    Lib-App home-page and search-page

---
+++ 03-frontend/react-library/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { HomePage } from './layouts/HomePage/HomePage';
+import { SearchBookPage } from './layouts/SearchBookPage/SearchBookPage';
+
+export const App = () => {
+  return (
+    <div>
+      <Navbar />
+      {/* <HomePage /> */}
+      <SearchBookPage />
+      <Footer />
+    </div>
+  );
+}
