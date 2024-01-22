8fbf3a865f29d4ee53fbbfd544d9a9a1633a4810
rajeev1209 <kumar.rajeevranjan10@yahoo.com>
2024-01-21 20:52:11 -0500

    Review see, add and submit

---
+++ 02-backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/ReviewService.java
@@ -0,0 +1,47 @@
+package com.luv2code.springbootlibrary.service;
+
+import com.luv2code.springbootlibrary.dao.BookRepository;
+import com.luv2code.springbootlibrary.dao.ReviewRepository;
+import com.luv2code.springbootlibrary.entity.Review;
+import com.luv2code.springbootlibrary.requestmodels.ReviewRequest;
+import net.bytebuddy.implementation.bytecode.Throw;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+@Service
+@Transactional
+public class ReviewService {
+
+    private ReviewRepository reviewRepository;
+
+    public ReviewService(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    public void postReview(String userEmail, ReviewRequest reviewRequest) throws Exception {
+        Review validateReview = reviewRepository.findByUserEmailAndBookId(userEmail, reviewRequest.getBookId());
+        if (validateReview != null) {
+            throw new Exception("Review already create");
+        }
+
+        Review review = new Review();
+        review.setBookId(reviewRequest.getBookId());
+        review.setRating(reviewRequest.getRating());
+        review.setUserEmail(userEmail);
+        reviewRequest.getReviewDescription().ifPresent(review::setReviewDescription);
+        review.setDate(Date.valueOf(LocalDate.now()));
+        reviewRepository.save(review);
+    }
+
+    public Boolean userReviewListed(String userEmail, Long bookId) {
+        Review userReview = reviewRepository.findByUserEmailAndBookId(userEmail, bookId);
+        if (userReview != null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
--- 03-frontend/react-library/src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
+++ 03-frontend/react-library/src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import BookModel from "../../models/BookModel";
+import { LeaveAReview } from "../Utils/LeaveAReview";
 
 export const CheckoutAndReviewBox: React.FC<{
     book: BookModel | undefined, mobile: boolean, currentLoansCount: number,
-    isAuthenticated: any, isCheckedOut: boolean, checkoutBook: any
+    isAuthenticated: any, isCheckedOut: boolean, checkoutBook: any,
+    isReviewLeft: boolean, submitReview: any
 }> = (props) => {
 
 
@@ -20,6 +22,28 @@ export const CheckoutAndReviewBox: React.FC<{
         return (<Link to={'/login'} className="btn btn-success btn-lg">Sign in</Link>)
     }
 
+    function reviewRender() {
+        if (props.isAuthenticated && !props.isReviewLeft) {
+            return (
+                <p>
+                    <LeaveAReview submitReview={props.submitReview} />
+                </p>
+            );
+        } else if (props.isAuthenticated && props.isReviewLeft) {
+            return (
+                <p>
+                    <b>Thank you for your review!</b>
+                </p>
+            )
+        }
+
+        return (
+            <div>
+                <hr />
+                <p>Sign in to be able to leave a review.</p>
+            </div>
+        )
+    }
     return (
         <div className={props.mobile ? 'card d-flex mt-5' : 'card col-3 container d-flex mb-5'} >
             <div className="card-body container">
@@ -51,7 +75,7 @@ export const CheckoutAndReviewBox: React.FC<{
                 {buttonRender()}
                 <hr />
                 <p className="mt-3">This number can change until placing order has been complete.</p>
-                <p> Sign in to be able to leave a review.</p>
+                {reviewRender()}
             </div>
         </div>
     );
